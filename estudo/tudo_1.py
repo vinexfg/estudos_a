bd4ac9f1376e2ecad5c3d265d0b6d1198b16aa74
Vinicius Almeida <vinysilvaa20@gmail.com>
2025-03-08 17:27:43 -0300

    a

--- estudo/tudo_1.py
+++ estudo/tudo_1.py
@@ -1,26 +1,21 @@
+
 import json
 
-
-produtos = [
-    {"produto": "Dell Laptop", "Preço": 15000},
-    {"produto": "HP Desktop", "Preço": 12000},
-    {"produto": "MacBook Pro", "Preço": 25000},
-    {"produto": "Lenovo ThinkPad", "Preço": 13000},
-    {"produto": "Acer Monitor", "Preço": 3000},
-    {"produto": "Samsung SSD", "Preço": 5000},
-    {"produto": "Logitech Mouse", "Preço": 150},
-    {"produto": "Logitech Teclado", "Preço": 200},
-    {"produto": "Canon Printer", "Preço": 3500},
-    {"produto": "Sony Headphones", "Preço": 1200},
-    {"produto": "Bose Speakers", "Preço": 5000},
-    {"produto": "Microsoft Surface", "Preço": 22000}
-]
+computador_json = """{
+    "Marca": "Dell",
+    "Preço": 15000   
+}"""
 
 
-# for produto in produtos:
-#     print(f'O produto {produto['produto']}, preço: {produto['Preço']}')
 
+dados = json.loads(computador_json)
 
-primeiro_produto = produtos[0]
+# salvando arquivo json
+with open('computador.json', 'w', encoding= 'utf-8')as arquivo_json:
+    json.dump(computador_json,arquivo_json)
+# lendo um arquivo json
 
-print(f'O produto {primeiro_produto['produto']}, preço: {primeiro_produto['Preço']}')
+with open('computador.json', encoding= 'utf-8') as arquivo_json:
+  string_json = json.load(arquivo_json) # aqui estou convertendo para de um arquivo json para string
+  dicionario_computador_json = json.loads(string_json)  # transformando string para dicionario, facilita na leitura
+  print(dicionario_computador_json['Marca'])  
